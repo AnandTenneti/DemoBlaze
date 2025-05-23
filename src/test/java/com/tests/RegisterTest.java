08a50d5eda98399e4c0fb4a731ec491a166bfe03
AnnadTenneti <tenneti.anand@gmail.com>
2025-05-24 01:23:15 +0530

    Adding RegisterTest.java file

--- src/test/java/com/tests/RegisterTest.java
+++ src/test/java/com/tests/RegisterTest.java
@@ -1,5 +1,6 @@
 package com.tests;
 
+import com.pages.RegistrationPage;
 import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.*;
 import org.openqa.selenium.chrome.ChromeDriver;
@@ -39,11 +40,32 @@ public class RegisterTest {
         Thread.sleep(5000);
     }
 
+    @Test(priority = 2)
+    public void testLoginWithInvalidCredentials() throws InterruptedException {
+        driver.get("https://www.demoblaze.com/");
+        System.out.println("Title of the page is: " + driver.getTitle());
+        driver.findElement(By.linkText("Sign up")).click();
+        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+        wait.until(ExpectedConditions.visibilityOfElementLocated(By.id("signInModalLabel")));
+        Assert.assertEquals(driver.findElement(
+                        By.id("signInModalLabel")).isDisplayed(), true,
+                "Sign in modal is not displayed");
+        RegistrationPage registrationPage = new RegistrationPage(driver);
+        registrationPage.clickOnSignUpButton();
+        String alertMessageText = registrationPage.getAlertMessageText();
+        Assert.assertEquals(alertMessageText, "Please fill out Username and Password.",
+                "Alert text is not matching");
+        Thread.sleep(5000);
+        registrationPage.acceptAlert();
+        Thread.sleep(3000);
+    }
+
     @AfterMethod
     public void tearDown() {
         System.out.println("Tearing down the test environment...");
         driver.quit();
     }
+
     public static void highlight(WebDriver driver, WebElement element) {
         JavascriptExecutor js = (JavascriptExecutor) driver;
         js.executeScript("arguments[0].setAttribute('style', 'background: yellow; border: 2px solid red;');", element);
