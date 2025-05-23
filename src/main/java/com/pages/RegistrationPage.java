2b7d93fbb5cdf4a3a7aeed8efc18b1f7f69ae511
AnnadTenneti <tenneti.anand@gmail.com>
2025-05-24 01:22:15 +0530

    Adding RegistrationPage.java file

--- src/main/java/com/pages/RegistrationPage.java
+++ src/main/java/com/pages/RegistrationPage.java
@@ -1,4 +1,6 @@
 package com.pages;
+
+import org.openqa.selenium.Alert;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
@@ -7,11 +9,13 @@ import org.openqa.selenium.support.PageFactory;
 
 public class RegistrationPage {
     WebDriver driver;
+    Alert alert;
 
     public RegistrationPage(WebDriver driver) {
         this.driver = driver;
         PageFactory.initElements(driver, this);
     }
+
     @FindBy(id = "sign-username")
     private WebElement userName;
 
@@ -21,10 +25,38 @@ public class RegistrationPage {
     @FindBy(xpath = "//button[contains(text(),'Sign up')]")
     private WebElement signUpButton;
 
-    @FindBy(css="button[text()='Close']")
+    @FindBy(css = "button[text()='Close']")
     private WebElement closeButton;
 
-    
+    @FindBy(id = "signInModalLabel")
+    private WebElement signInModalLabel;
 
+public void clickOnSignUpButton() {
+        signUpButton.click();
+    }
 
+    public void enterUserName(String username) {
+        userName.sendKeys(username);
+    }
+
+    public void enterPassword(String password) {
+        this.password.sendKeys(password);
+    }
+    public String getAlertMessageText() {
+    alert = driver.switchTo().alert();
+        String alertText = alert.getText();
+        System.out.println("Alert text: " + alertText);
+        return alertText;
+    }
+    public void acceptAlert() {
+        alert.accept();
+    }
+
+    public void clickOnCloseButton() {
+        closeButton.click();
+    }
+
+    public String getSignInModalLabelText() {
+        return signInModalLabel.getText();
+    }
 }
