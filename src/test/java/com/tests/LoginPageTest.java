0dc4b36e781dc1fc84ec3f13fd3486f0104e0f88
AnnadTenneti <tenneti.anand@gmail.com>
2025-06-11 08:30:43 +0530

    Adding LoginPageTest.java file

---
+++ src/test/java/com/tests/LoginPageTest.java
@@ -0,0 +1,29 @@
+package com.tests;
+
+import com.pages.HeaderPage;
+import com.pages.LoginPage;
+import org.apache.hc.core5.http.Header;
+import org.testng.annotations.Test;
+
+public class LoginPageTest extends BaseTest{
+
+
+    @Test
+    public void test_LoginPage() throws InterruptedException {
+        HeaderPage headerPage = new HeaderPage(driver);
+        System.out.println("Navigating to Login Page...");
+        headerPage.clickOnLogInLink();
+        Thread.sleep(2000);
+        LoginPage loginPage = new LoginPage(driver);
+        System.out.println("Entering username and password...");
+        loginPage.enterUserName("test@testone.com");
+        loginPage.enterPassword("test123");
+        System.out.println("Clicking on Login button...");
+        loginPage.clickOnLoginButton();
+        Thread.sleep(5000);
+        System.out.println("Verifying the page title...");
+        headerPage.clickOnLogOutLink();
+        Thread.sleep(5000);
+
+    }
+}
