aafb43f3c4434b7e45c6655731e63aea09da8e49
AnnadTenneti <tenneti.anand@gmail.com>
2025-06-11 08:34:21 +0530

    Adding HomePageTest.java file

--- src/test/java/com/tests/HomePageTest.java
+++ src/test/java/com/tests/HomePageTest.java
@@ -1,5 +1,7 @@
 package com.tests;
 
+import com.pages.HomePage;
+import com.pages.ProductDetailsPage;
 import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.*;
 import org.openqa.selenium.chrome.ChromeDriver;
@@ -7,69 +9,49 @@ import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.Assert;
-import org.testng.annotations.BeforeMethod;
-import org.testng.annotations.BeforeTest;
-import org.testng.annotations.Test;
+import org.testng.annotations.*;
 
 import java.time.Duration;
 
 public class HomePageTest extends BaseTest {
 
-    WebDriver driver;
-
-    @BeforeTest
-    public void setUp() {
-        driver = initDriver();
-    }
-
-    @BeforeMethod
-    public void setBaseURL() {
-        System.out.println("Setting up the base URL...");
-        driver.get("https://www.demoblaze.com/");
-    }
-
-
-    @Test
-    public void addProductToCart() throws InterruptedException {
+    @Test(priority = 1, dataProvider = "CPnames", dataProviderClass = DataSupplier.class,
+            description = "Add a product to the cart from a specific category")
+    public void addProductToCart(String categoryName, String productName) throws InterruptedException {
         System.out.println("Adding product to cart...");
-        // Add your test code here
-        driver.get("https://www.demoblaze.com/index.html");
-        System.out.println("Title of the page is: " + driver.getTitle());
-        driver.findElement(By.linkText("Laptops")).click();
+
+        HomePage homePage = new HomePage(driver);
+        homePage.clickOnACategory(categoryName);
         Thread.sleep(2000);
-        driver.findElement(By.linkText("Sony vaio i7")).click();
-        //String headerText = driver.findElement(By.cssSelector("h2[class='name']")).getText();
-        // Assert.assertEquals(headerText, "Sony vaio i7",
-        //       "Product header text does not match");
-        Assert.assertEquals(driver.findElement(By.linkText("Add to cart")).isDisplayed(), true,
-                "Add to cart button is not displayed");
-        driver.findElement(By.linkText("Add to cart")).click();
+        homePage.clickOnAProduct(productName);
         Thread.sleep(2000);
-        Alert alert = driver.switchTo().alert();
-        String alertText = alert.getText();
+        ProductDetailsPage productDetailsPage = new ProductDetailsPage(driver);
+        productDetailsPage.clickAddToCart();
+
+        String alertText = productDetailsPage.getAlertMessageText();
         Assert.assertEquals(alertText, "Product added", "Alert text is not matching");
-        Thread.sleep(4000);
-        alert.accept();
+        Thread.sleep(3000);
+        productDetailsPage.acceptAlert();
     }
 
-    @Test(priority = 3)
-    public void scrollToBottom() throws InterruptedException {
+    @Test(priority = 2)
+    public void test_scrollToBottom() throws InterruptedException {
         System.out.println("Scrolling to the bottom of the page...");
-        JavascriptExecutor js = (JavascriptExecutor) driver;
-        js.executeScript("window.scrollTo(0, document.body.scrollHeight);");
+        scrollToBottom();
         Thread.sleep(2000);
-        Assert.assertTrue(driver.findElement(By.id("next2")).isDisplayed(),
+        HomePage homePage = new HomePage(driver);
+        Assert.assertTrue(homePage.isNextButtonDisplayed(),
                 "Next button is not displayed at the bottom of the page");
     }
 
-    @Test(priority = 4)
-    public void scrollToElementView() throws InterruptedException {
+    @Test(priority = 3, dataProvider = "productName", dataProviderClass = DataSupplier.class,
+            description = "Scroll to the product element and click on it")
+    public void scrollToElementView(String productName) throws InterruptedException {
         System.out.println("Scrolling to element view...");
         Thread.sleep(3000);
-        WebElement productLink = driver.findElement(By.xpath("//a[text()='Sony vaio i5']"));
-        JavascriptExecutor js = (JavascriptExecutor) driver;
-
-        js.executeScript("arguments[0].scrollIntoView(true);", productLink);
+        WebElement productLink =
+                driver.findElement(By.xpath("//a[text()='" + productName + "']"));
+        scrollElementIntoView(productLink);
         Thread.sleep(3000);
         Assert.assertTrue(productLink.isDisplayed(), "Product link is not displayed in view");
         productLink.click();
@@ -82,8 +64,9 @@ public class HomePageTest extends BaseTest {
         int productPrice =
                 Integer.parseInt(priceOfProduct);
         System.out.println("The price of the product is " + productPrice);
-//        Assert.assertEquals(driver.getTitle(), "Sony vaio i5", "Product page title does not match");
         Thread.sleep(3000);
     }
+
+
 
 }
