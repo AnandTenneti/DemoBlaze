0d0c091862c484e24032ff5251cd8e2ed52de577
AnnadTenneti <tenneti.anand@gmail.com>
2025-06-12 23:27:21 +0530

    Adding changes in DriverManager.java file

--- src/test/java/com/driver/DriverManager.java
+++ src/test/java/com/driver/DriverManager.java
@@ -3,14 +3,18 @@ package com.driver;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.firefox.FirefoxDriver;
 
 public class DriverManager {
     static WebDriver driver;
+
     public static WebDriver initDriver() {
         System.out.println("Setting up the test environment...");
-        WebDriverManager.chromedriver().setup();
+        //WebDriverManager.chromedriver().setup();
+        WebDriverManager.firefoxdriver().setup();
         System.out.println("WebDriver setup complete.");
-        driver = new ChromeDriver();
+        //driver = new ChromeDriver();
+        driver = new FirefoxDriver();
         driver.manage().window().maximize();
         return driver;
     }
