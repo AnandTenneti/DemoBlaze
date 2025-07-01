0504489eac8f56da2bb3f3b46ea1fd7f73f783b1
AnnadTenneti <tenneti.anand@gmail.com>
2025-07-01 19:25:28 +0530

    Adding DriverManager.java file

--- src/test/java/com/driver/DriverManager.java
+++ src/test/java/com/driver/DriverManager.java
@@ -4,7 +4,7 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.chrome.ChromeOptions;
-import org.openqa.selenium.firefox.FirefoxDriver;
+
 
 public class DriverManager {
     static WebDriver driver;
@@ -12,11 +12,11 @@ public class DriverManager {
     public static WebDriver initDriver() {
         System.out.println("Setting up the test environment...");
         WebDriverManager.chromedriver().setup();
-        //WebDriverManager.firefoxdriver().setup();
         System.out.println("WebDriver setup complete.");
         ChromeOptions options = new ChromeOptions();
         options.addArguments("--start-maximized");
-        options.addArguments("--headless");// Uncomment this line to run in headless mode
+
+        //options.addArguments("--headless");// Uncomment this line to run in headless mode
         driver = new ChromeDriver(options);
 
 
