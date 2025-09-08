130026c2a622fcd2a312251257a0d736b64fce63
AnnadTenneti <tenneti.anand@gmail.com>
2025-09-08 15:39:13 +0530

    Updated the changes

--- src/test/java/com/driver/DriverManager.java
+++ src/test/java/com/driver/DriverManager.java
@@ -1,23 +1,40 @@
 package com.driver;
 
+import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
-import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.firefox.FirefoxOptions;
 
 
 public class DriverManager {
     static WebDriver driver;
 
-    public static WebDriver initDriver() {
+    public static WebDriver initDriver(String browserName) {
+        switch (browserName.toLowerCase()) {
+            case "chrome":
+                WebDriverManager.chromedriver().setup();
+                ChromeOptions options = new ChromeOptions();
+                options.addArguments("--start-maximized");
+                driver = new ChromeDriver(options);
+                break;
+            case "firefox":
+                WebDriverManager.firefoxdriver().setup();
+                driver = new org.openqa.selenium.firefox.FirefoxDriver();
+                FirefoxOptions firefoxOptions = new FirefoxOptions();
+                //firefoxOptions.addArguments("--headless");
+
+                break;
+            // Add cases for other browsers if needed
+            default:
+                throw new IllegalArgumentException("Browser not supported: " + browserName);
+        }
         System.out.println("Setting up the test environment...");
-        WebDriverManager.chromedriver().setup();
+
         System.out.println("WebDriver setup complete.");
-        ChromeOptions options = new ChromeOptions();
-        options.addArguments("--start-maximized");
+
 
         //options.addArguments("--headless");// Uncomment this line to run in headless mode
-        driver = new ChromeDriver(options);
 
 
         return driver;
